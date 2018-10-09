53b93e269b0403d8cb82246e173ebe5008815fdb
yzzxw <88615442@qq.com>
2018-10-09 22:04:16 +0800

    登录和注销状态切换

--- zlktqa.py
+++ zlktqa.py
@@ -54,11 +54,21 @@ def regist():
                 # 如果注册成功，就让页面跳转到登录的页面
                 return redirect(url_for('login'))
 
+@app.route('/logout/')
+def logout():
+    # session.pop('user_id')
+    # del session('user_id')
+    session.clear()
+    return redirect(url_for('login'))
 
-
-
-
-
+@app.context_processor
+def my_context_processor():
+    user_id = session.get('user_id')
+    if user_id:
+        user = User.query.filter(User.id == user_id).first()
+        if user:
+            return {'user':user}
+    return {}
 
 
 if __name__ == '__main__':
