0bdba6bd763b5b7c7920404a3f4ffc848d161176
yzzxw <88615442@qq.com>
2018-10-09 23:15:31 +0800

    发布问答界面完成

--- zlktqa.py
+++ zlktqa.py
@@ -61,6 +61,13 @@ def logout():
     session.clear()
     return redirect(url_for('login'))
 
+@app.route('/question/')
+def question():
+    if request.method == 'GET':
+        return render_template('question.html')
+    else:
+        pass
+
 @app.context_processor
 def my_context_processor():
     user_id = session.get('user_id')
